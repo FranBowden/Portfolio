782fb22d795b9e96d4879ccf998909b732e13c05
FranBowden <francesca.bowden22@bathspa.ac.uk>
2023-05-19 00:02:49 +0100

    Add Scroll Up Button

--- Javascript/index.js
+++ Javascript/index.js
@@ -84,3 +84,51 @@ const left = new IntersectionObserver(entries => {
 });
 
 left.observe(document.querySelector('.left-wrapper'));
+<<<<<<< Updated upstream
+=======
+*/
+
+
+
+
+// Select the element
+const element = document.getElementsByClassName('right-to-left-slide');
+
+// Function to handle scroll event
+function handleScroll() {
+  // Calculate the scroll amount
+  const scrollAmount = window.scrollX;
+
+  // Update the left position of the element
+  element.style.left = -scrollAmount + 'px';
+}
+
+// Attach scroll event listener
+window.addEventListener('scroll', handleScroll);
+
+
+
+//scroll up button
+let scrollUpBtn = document.getElementById("scroll-up-btn");
+
+// When the user scrolls down 800px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//text to speech 
+>>>>>>> Stashed changes
