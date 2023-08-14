22451fca1079fbe37ad9c7f870f00bb6f3326ee2
FranBowden <francesca.bowden22@bathspa.ac.uk>
2023-08-14 14:34:49 +0100

    Organised Code

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,4 +1,5 @@
 //Slide Headers in on Scroll
+
 window.addEventListener('scroll', function() {
   const slideText = document.getElementById('hello-title');
   const slideText2 = document.getElementById('name-title');
@@ -15,6 +16,7 @@ window.addEventListener('scroll', function() {
   }
 });
 
+/*
 // Intersection Observer API
 function toggleTextAnimation() {const observer = new IntersectionObserver(entries => {
   console.log(entries)
@@ -31,36 +33,29 @@ hiddenElements.forEach((el) => observer.observe(el));
 }
 
 toggleTextAnimation() //calling toggle animation function
-
-
-
-//Creates a Parallax effect with GSAP
-gsap.to("#fireflies", {
-  scrollTrigger: {
-      scrub: true
-  },
-  x: 200,
-  scale: 1.2
-})
-
-gsap.to("#lights", {
-  scrollTrigger: {
-      scrub: true
-  },
-  x: 200,
-  scale: 1.2
-})
-
-
-//speechSynthesis API - Text to Speech Function
-function textToSpeech(ref) {
-  const synth = window.speechSynthesis; //speech service
-  synth.speak(new SpeechSynthesisUtterance(document.getElementById(ref).innerHTML)); //gets ref and gets the innerhtm text 
-}
-
+*/
 
 //Scroll to Top Button
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".hidden");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("show");
+    } else {
+      reveals[i].classList.remove("show");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
