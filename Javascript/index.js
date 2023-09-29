e854e80bae8f48cb8c0206e81ebc9154047acd0a
FranBowden <francesca.bowden22@bathspa.ac.uk>
2023-09-29 21:15:08 +0100

    Portfolio Redesigned

--- Javascript/index.js
+++ Javascript/index.js
@@ -48,12 +48,3 @@ reveals.forEach(reveal => {
 
 
 
-
-
-$('#col-container').waypoint(function() {
-  $(".col-3").css({ 
-        opacity: "1",
-        marginTop: "0"
-      });
-}, { offset: 150 });
-
